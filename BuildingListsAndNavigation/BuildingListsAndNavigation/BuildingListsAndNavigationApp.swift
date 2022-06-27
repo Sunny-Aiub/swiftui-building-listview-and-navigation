c78b83d3e508510714e0fd0d61d4da2bafab501d
chowdhury_shaheb <mhcsunny@gmail.com>
2022-06-27 17:23:39 +0600

    files added

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsAndNavigationApp.swift
+//  BuildingListsAndNavigation
+//
+//  Created by MACBOOK Pro - WSIT on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
